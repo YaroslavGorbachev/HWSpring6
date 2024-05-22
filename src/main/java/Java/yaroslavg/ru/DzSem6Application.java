adc29d8912feff04b160a939098a0d288bad79bd
Yaroslav <rus070392@yandex.ru>
2024-05-22 14:34:54 +0700

    HW6

---
+++ src/main/java/Java/yaroslavg/ru/DzSem6Application.java
@@ -0,0 +1,13 @@
+package Java.yaroslavg.ru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzSem6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzSem6Application.class, args);
+	}
+
+}
